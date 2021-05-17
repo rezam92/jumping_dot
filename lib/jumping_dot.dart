fbc9d162074dae00c6860e187a68b831ac96ffd3
Reza <rm.1992@yahoo.com>
2021-05-17 21:01:13 +0430

    add null safety

--- lib/jumping_dot.dart
+++ lib/jumping_dot.dart
@@ -2,11 +2,11 @@ library jumping_dot;
 import 'package:flutter/material.dart';
 
 class DotWidget extends StatelessWidget {
-  final Color color;
-  final double radius;
+  final Color? color;
+  final double? radius;
 
   const DotWidget({
-    Key key,
+    Key? key,
     @required this.color,
     @required this.radius,
   }) : super(key: key);
@@ -32,7 +32,7 @@ class JumpingDots extends StatefulWidget {
   final double radius;
 
   const JumpingDots({
-    Key key,
+    Key? key,
     this.numberOfDots = 3,
     this.radius = 10,
     this.color = const Color(0xfff2c300),
@@ -44,7 +44,7 @@ class JumpingDots extends StatefulWidget {
 
 class _JumpingDotsState extends State<JumpingDots>
     with TickerProviderStateMixin {
-  List<AnimationController> _animationControllers;
+  List<AnimationController>? _animationControllers;
 
   List<Animation<double>> _animations = [];
 
@@ -58,15 +58,13 @@ class _JumpingDotsState extends State<JumpingDots>
 
   @override
   void dispose() {
-    for (var controller in _animationControllers) {
+    for (var controller in _animationControllers!) {
       controller.dispose();
     }
     super.dispose();
   }
 
   void _initAnimation() {
-    ///initialization of _animationControllers
-    ///each _animationController will have same animation duration
     _animationControllers = List.generate(
       widget.numberOfDots,
           (index) {
@@ -75,37 +73,26 @@ class _JumpingDotsState extends State<JumpingDots>
       },
     ).toList();
 
-    ///initialization of _animations
-    ///here end value is -20
-    ///end value is amount of jump.
-    ///and we want our dot to jump in upward direction
     for (int i = 0; i < widget.numberOfDots; i++) {
       _animations.add(
-          Tween<double>(begin: 0, end: -20).animate(_animationControllers[i]));
+          Tween<double>(begin: 0, end: -20).animate(_animationControllers![i]));
     }
 
     for (int i = 0; i < widget.numberOfDots; i++) {
-      _animationControllers[i].addStatusListener((status) {
-        //On Complete
+      _animationControllers![i].addStatusListener((status) {
         if (status == AnimationStatus.completed) {
-          //return of original postion
-          _animationControllers[i].reverse();
-          //if it is not last dot then start the animation of next dot.
+          _animationControllers![i].reverse();
           if (i != widget.numberOfDots - 1) {
-            _animationControllers[i + 1].forward();
+            _animationControllers![i + 1].forward();
           }
         }
-        //if last dot is back to its original postion then start animation of the first dot.
-        // now this animation will be repeated infinitely
         if (i == widget.numberOfDots - 1 &&
             status == AnimationStatus.dismissed) {
-          _animationControllers[0].forward();
+          _animationControllers![0].forward();
         }
       });
     }
-
-    //trigger animtion of first dot to start the whole animation.
-    _animationControllers.first.forward();
+    _animationControllers!.first.forward();
   }
 
   @override
@@ -116,17 +103,11 @@ class _JumpingDotsState extends State<JumpingDots>
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: List.generate(widget.numberOfDots, (index) {
-            //AnimatedBuilder widget will rebuild it self when
-            //_animationControllers[index] value changes.
             return AnimatedBuilder(
-              animation: _animationControllers[index],
+              animation: _animationControllers![index],
               builder: (context, child) {
                 return Container(
                   padding: EdgeInsets.all(2.5),
-                  //Transform widget's translate constructor will help us to move the dot
-                  //in upward direction by changing the offset of the dot.
-                  //X-axis position of dot will not change.
-                  //Only Y-axis position will change.
                   child: Transform.translate(
                     offset: Offset(0, _animations[index].value),
                     child: DotWidget(color: widget.color, radius: widget.radius),
